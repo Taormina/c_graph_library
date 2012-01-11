c20b2712af2d775ef63bba97f7380084137e609d
CaptainAndred <danieljwhatley@gmail.com>
2012-01-11 01:45:45 -0500

    Added heap declarations to graph.h, shortened unnecessarily long comment sections

--- graph.h
+++ graph.h
@@ -1,21 +1,15 @@
-/*
- *  Vertex struct 
- */
+/* Vertex struct */
 typedef struct cgl_vertex {
 	char* label;
 } vertex;
 
-/*
- *  Edge struct 
- */
+/* Edge struct */
 typedef struct cgl_edge {
 	vertex* u, v;
 	signed int weight;
 } edge;
 
-/*
- *  Graph struct 
- */
+/* Graph struct */
 typedef struct cgl_graph {
 	edge* min_heap;
 	int size;
@@ -24,4 +18,6 @@ typedef struct cgl_graph {
 } graph;
 
 
-
+/*  Heap declarations */
+edge* heapify(edge* min_heap);
+int regrow(edge* min_heap);
